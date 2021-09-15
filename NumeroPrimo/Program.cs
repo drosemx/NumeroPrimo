a4d98e1263bf2ac75019047fcb5c87c00c40c5a1
Dirk E. Rose Dominguez <DIrk_Rose@jabil.com>
2021-09-15 13:35:44 -0500

    Add project files.

---
+++ NumeroPrimo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace NumeroPrimo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            if (EsPrimo(num))
+            {
+                Console.WriteLine("Es primo");
+            }
+            else
+            {
+                Console.WriteLine("No es primo");
+            }
+        }
+        static bool EsPrimo(int numero)
+        {
+            for (int i = 2; i < numero; i++)
+            {
+                if ((numero % i) == 0)
+                {
+                    // No es primo :(
+                    return false;
+                }
+            }
+            // Es primo :)
+            return true;
+
+        }
+    }
+}
